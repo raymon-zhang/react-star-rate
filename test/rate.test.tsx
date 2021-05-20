c03414e025c5a9c8b6792927059ebdf82003c33d
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-05-20 15:55:54 -0700

    Fixed tests

--- test/rate.test.tsx
+++ test/rate.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import StarsRating from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<StarsRating />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
